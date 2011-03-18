99b237f115728ddd1bd98d842914f19c2c3605ce
Will Barrett <william.barrett@tallgreentree.com>
2011-03-18 07:26:04 -0500

    Clean project with Devise, Haml, jQuery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Expensebalance::Application.config.secret_token = 'ff0730ff8ad36444c7eebcc5dbb51b6cb529c2f63a4e708599b69e948753c4b517ab4738a9ff4b421b316ae9f37dbdfe50c324b64f76b55797effe414a788769'
