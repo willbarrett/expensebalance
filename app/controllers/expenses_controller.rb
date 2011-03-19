3b2b212b9538dd62b990c2fc4a3a0f1f8b6f1083
Will Barrett <william.barrett@tallgreentree.com>
2011-03-19 16:08:18 -0500

    Tweaking some math.

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -89,7 +89,7 @@ class ExpensesController < ApplicationController
     def find_net
       mine = Expense.where(:user_id => current_user.id).sum('amount')
       theirs = Expense.where('user_id <> ?', current_user.id).sum('amount')
-      @net = theirs - mine
+      @net = mine - theirs
       @net_color = @net >= 0 ? 'color: green' : 'color: red'
     end
 end
