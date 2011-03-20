030483b8167feb8f7ae860045b868f2f273b7c77
Will Barrett <william.barrett@tallgreentree.com>
2011-03-19 19:15:04 -0500

    Forcing dollar format on number strings, adding validations.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,6 @@
 class Expense < ActiveRecord::Base
   belongs_to :user
   default_scope order 'date DESC'
+  validates_presence_of :amount, :for
+  validates_format_of :amount, :with => /\d+?(\.\d+)/, :message => "must be a dollar amount"
 end
