4a98b2b73c3504ad43ecb1758e51cac112482093
Will Barrett <william.barrett@tallgreentree.com>
2011-04-06 18:00:53 -0500

    Added new home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Expensebalance::Application.routes.draw do
+  get "welcome/index"
+
   resources :expenses
 
   devise_for :users
@@ -52,7 +54,7 @@ Expensebalance::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "expenses#index"
+  root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
